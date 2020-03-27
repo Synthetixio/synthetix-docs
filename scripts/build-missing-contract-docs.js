4af59c99ef2df364dba0d1dd7493cedd1cba39c6
justin j. moses <justinjmoses@gmail.com>
2020-03-27 14:10:06 -0400

    AST parser WIP

--- scripts/build-missing-contract-docs.js
+++ scripts/build-missing-contract-docs.js
@@ -2,11 +2,15 @@
 
 const path = require('path');
 const fs = require('fs');
+const axios = require('axios');
+const parser = require('solidity-parser-antlr');
 
 const snx = require('synthetix');
 
 const network = 'mainnet';
 const sources = snx.getSource({ network });
+
+const { version } = require('./utils');
 
 const getPathFromSource = ({ source }) => path.join(__dirname, '..', 'content', 'contracts', `${source}.md`);
 const doesContractExist = ({ source }) => fs.existsSync(getPathFromSource({ source }));
@@ -14,13 +18,31 @@ const doesContractExist = ({ source }) => fs.existsSync(getPathFromSource({ sour
 (async () => {
 	for (const [source] of Object.entries(sources)) {
 		if (doesContractExist({ source })) {
-			continue;
+			// continue;
+		}
+
+		const currentVersion = 'v' + version();
+
+		const { data: soliditySource } = await axios.get(
+			`https://raw.githubusercontent.com/Synthetixio/synthetix/${currentVersion}/contracts/${source}.sol`,
+		);
+
+		try {
+			fs.writeFileSync(
+				path.join(__dirname, source + '.json'),
+				JSON.stringify(parser.parse(soliditySource), null, '\t'),
+			);
+			// TODO
+		} catch (e) {
+			if (e instanceof parser.ParserError) {
+				console.log(e.errors);
+			}
 		}
 
 		const content = `
 # ${source}
 
-**Source:** [${source}.sol](https://github.com/Synthetixio/synthetix/blob/master/contracts/${source}.sol)
+**Source:** [${source}.sol](https://github.com/Synthetixio/synthetix/blob/${currentVersion}/contracts/${source}.sol)
 
 !!! todo "Work in Progress"
 
